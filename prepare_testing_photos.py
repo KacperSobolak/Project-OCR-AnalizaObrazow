ceb7037660a3b823fe6a8044d3a58f2dd3af610e
Joanna <joanna.heldak@gmail.com>
2025-01-24 11:00:21 +0100

    created_model_and_tested_works_great_but_not_our_data

---
+++ prepare_testing_photos.py
@@ -0,0 +1,40 @@
+import cv2
+import glob
+import numpy as np
+import os
+from tqdm import tqdm
+
+input_dir='TrainLetters/'
+output_dir='BinaryTrainLetters/'
+
+new_width = 50
+new_height = 100
+
+
+def process_images(input_dir, output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # Process each subdirectory in the input directory + progress bar by wrapping iterable into tqdm
+    for subdir in tqdm(os.listdir(input_dir), desc="Processing directories"):
+        sub_input_path = os.path.join(input_dir, subdir)
+        sub_output_path = os.path.join(output_dir, subdir)
+
+        if not os.path.exists(sub_output_path):
+            os.makedirs(sub_output_path)
+
+        for file_path in glob.glob(os.path.join(sub_input_path, "*.jpg")):
+            img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
+            resized_img = cv2.resize(img, (new_width, new_height))
+            #blurred= cv2.GaussianBlur(resized_img, (5, 5), 0)
+            #thresh = cv2.adaptiveThreshold(resized_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 10)
+            #kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+            #binary_img = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=1)
+            
+            output_file_path = os.path.join(sub_output_path, os.path.basename(file_path))
+
+            cv2.imwrite(output_file_path, resized_img)
+
+    print("All images have been processed and saved in:", output_dir)
+
+process_images(input_dir, output_dir)
--- recognize.py
+++ recognize.py
@@ -4,23 +4,24 @@ import matplotlib.pyplot as plt
 import sys
 
 def preprocess_image(image_path):
-    image = cv2.imread(image_path, cv2.IMREAD_COLOR)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = cv2.imread(image_path, cv2.IMREAD_COLOR)    
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
 
-    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0) 
 
     thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 
-                                   cv2.THRESH_BINARY_INV, 15, 10)
+                                   cv2.THRESH_BINARY_INV, 15, 10)     
+    
 
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
     thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=1)
-    return image, thresh
+    return image, thresh  
 
 def extract_characters(image, contours):
     characters = []
     char_dimensions = []
 
-    image_height, image_width = image.shape[:2]
+    image_height, image_width = image.shape[:2] 
 
     for contour in contours:
         x, y, w, h = cv2.boundingRect(contour)
@@ -46,6 +47,7 @@ def find_contours(thresh):
     return contours
 
 def save_and_display_characters(characters):
+    plt.figure(figsize=(10, 5))
     for idx, char in enumerate(characters):
         char_resized = cv2.resize(char, (50, 100))
         plt.subplot(1, len(characters), idx + 1)
@@ -53,10 +55,18 @@ def save_and_display_characters(characters):
         plt.axis('off')
     plt.show()
 
+
 def main(image_path):
     image, thresh = preprocess_image(image_path)
+    plt.figure()
+    plt.imshow(thresh, cmap='gray')
     contours = find_contours(thresh)
-    characters = extract_characters(thresh, contours)
+    characters = extract_characters(thresh, contours)   # white images on black background
+    
+    
+    inverted_characters = [cv2.bitwise_not(char) for char in characters ]   # black images on white background
+
     save_and_display_characters(characters)
+    save_and_display_characters(inverted_characters)
 
 main(sys.argv[1])
